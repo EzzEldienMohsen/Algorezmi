ed163856dd007b1990097df8d878b8bf79b345bf
Ezz dghedy <ezzmohsend@gmail.com>
2024-05-10 13:31:31 +0200

    the middle section and the header adj

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,14 +4,31 @@ import React from 'react';
 import { logo } from '../assets';
 import { Link } from 'react-router-dom';
 import { Menu, NavBar } from '../subComponents';
+import { motion } from 'framer-motion';
 
 const Header = () => {
+    const [isScrolled, setIsScrolled] = React.useState(false);
+
   React.useEffect(() => {
     AOS.init();
     AOS.refresh();
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      const threshold = 480;
+      setIsScrolled(scrollPosition > threshold);}
+      window.addEventListener('scroll', handleScroll);
+
+      return () => {
+        window.removeEventListener('scroll', handleScroll);
+      };
   }, []);
   return (
-    <div className="fixed top-0 left-0 z-10 w-full shadow-xl border-b-[3px] border-[#495057]">
+    <motion.header
+      transition={{ duration: 0.5, ease: 'easeInOut' }}
+      initial="bg-transparent"
+      animate={isScrolled ? 'bg-white' : 'bg-transparent'}
+      className={`fixed top-0 left-0 z-10 w-full shadow-xl border-b-[3px] border-[#495057] ${isScrolled?"bg-white":"bg-transparent"}`}
+    >
       <div
         data-aos="fade-down"
         data-aos-duration="2000"
@@ -21,9 +38,9 @@ const Header = () => {
           <img src={logo} alt="logo" className="w-32" />
         </Link>
         <NavBar />
-        <Menu/>
+        <Menu />
       </div>
-    </div>
+    </motion.header>
   );
 };
 
--- src/subComponents/MiddleLanding.jsx
+++ src/subComponents/MiddleLanding.jsx
@@ -4,38 +4,38 @@ import  AOS  from 'aos';
 
 const MiddleLanding = () => {
     React.useEffect(()=>{
-        AOS.init()
+        AOS.init();
+        AOS.refresh();
     },[])
   return (
-    <div className="bg-[#28283f] px-4 py-6 flex flex-col justify-center items-center text-white">
+    <div className="bg-[white] px-4 py-6 flex flex-col justify-center items-center text-white">
       <div className="theImage flex flex-col justify-center items-center my-6  p-8 w-full">
         <h1
           data-aos="zoom-out-up"
           data-aos-duration="2000"
-          className="font-Nunito text-[white] text-xl md:text-2xl lg:text-4xl mb-4 capitalize font-bold"
+          className="font-Nunito text-[#052c65] text-xl md:text-2xl lg:text-4xl mb-4 capitalize font-bold"
         >
           What We Offer
         </h1>
         <p
           data-aos="fade-up"
           data-aos-duration="2000"
-          className="font-Nunito text-[white] text-lg md:text-xl mb-8 capitalize lg:text-2xl font-medium"
+          className="font-Nunito text-[#052c65] text-lg md:text-xl mb-8 capitalize lg:text-2xl font-medium"
         >
           Rooted in mutual understanding and effective collaboration, our
           result-oriented teams offer these services to clients globally.
         </p>
         <div
           data-aos="fade-down-right"
-          data-aos-easing="ease-in-back"
           data-aos-delay="2000"
-          className="flex justify-between mb-4 items-center border-[#052c65] shadow-xl border-2 px-4 py-2 rounded-md"
+          className="flex justify-between mb-4 items-center border-[#0C71C3] shadow-xl border-2 px-4 py-2 rounded-md"
         >
           <img src={mobile2} alt="alt" className="w-20 md:w-32 lg:w-40" />
           <div className="flex flex-col justify-between items-start md:w-3/5 ">
-            <h1 className="text-[white] font-bold mb-2 md:mb-4 font-Nunito text-lg md:text-xl lg:text-2xl">
+            <h1 className="text-[#052c65] font-bold mb-2 md:mb-4 font-Nunito text-lg md:text-xl lg:text-2xl">
               MOBILE DEVELOPMENT
             </h1>
-            <p className="text-[white] font-light font-Nunito text-md md:text-lg lg:text-xl">
+            <p className="text-[#052c65] font-light font-Nunito text-md md:text-lg lg:text-xl">
               Equipped to deliver user-centric native and cross-platform mobile
               applications that work seamlessly across smartphones and
               wearables.
@@ -44,16 +44,15 @@ const MiddleLanding = () => {
         </div>
         <div
           data-aos="fade-up-left"
-          data-aos-easing="ease-in-back"
           data-aos-delay="2000"
-          className="flex justify-between flex-row-reverse items-center shadow-xl border-[#052c65] border-2 px-4 py-2 rounded-md"
+          className="flex justify-between flex-row-reverse items-center shadow-xl border-[#0C71C3] border-2 px-4 py-2 rounded-md"
         >
           <img src={web2} alt="alt" className="w-20 md:w-32 lg:w-40" />
           <div className="flex flex-col justify-between items-start md:w-3/5">
-            <h1 className="text-[white] font-bold mb-2 md:mb-4 font-Nunito text-lg md:text-xl lg:text-2xl">
+            <h1 className="text-[#052c65] font-bold mb-2 md:mb-4 font-Nunito text-lg md:text-xl lg:text-2xl">
               WEB DEVELOPMENT
             </h1>
-            <p className="text-[white] font-light font-Nunito text-md md:text-lg lg:text-xl">
+            <p className="text-[#052c65] font-light font-Nunito text-md md:text-lg lg:text-xl">
               Leverage modern web frameworks to develop and deploy feature-rich
               web apps that digitize your business services, expand your reach,
               and excel among competitors.
@@ -61,10 +60,7 @@ const MiddleLanding = () => {
           </div>
         </div>
       </div>
-      <img
-        src="https://algorezmi.com/wp-content/uploads/2023/03/app-19.png"
-        alt="image"
-      />
+     
     </div>
   );
 }
